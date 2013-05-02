85681bf7174ef77da69437b175799de8ae5cc477
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2013-05-02 17:33:39 -0400

    updated

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,7 @@
 if (ENV['REDISTOGO_URL'])
   uri = URI.parse(ENV['REDISTOGO_URL'])
-  Rails.logger.info ENV['REDISTOGO_URL']
-  Rails.logger.info uri
 
-  REDIS = Redis.new(host => uri.host, :port => uri.port, :password => uri.password)
+  REDIS = Redis.new(:host => uri.host, :user => uri.user, :port => uri.port, :password => uri.password)
 else
   REDIS = MockRedis.new
 end
